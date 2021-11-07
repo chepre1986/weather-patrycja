9f332652deeb0f601fe0c4b02c30097ce464c4a4
chepre1986 <chepre1986@gmail.com>
2021-11-07 19:13:20 +0100

    another try

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-      <footer className="App-footer">
+      <footer className="App-footer text-center">
                           <a className="source-link" title="source-link" href="https://github.com/chepre1986/weather-patrycja.git" target="_blank" rel="noreferrer">Open-source</a>
       {" "} by Patrycja Szumska.
       </footer>
