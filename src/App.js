034a8abb6ce09dafeab5e53047b11bc79c605d4f
chepre1986 <chepre1986@gmail.com>
2021-11-09 20:19:27 +0100

    Bootstrap works!!

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-
 import './App.css';
-import bootstrap from 'bootstrap' ;
+
 
 
 function App() {
   return (
     <div className="App">
       <div className="container">
+      <h1>
+     🌺Weather App🌺
+      </h1>
       <footer className="App-footer text-center">
                           <a className="source-link" title="source-link" href="https://github.com/chepre1986/weather-patrycja.git" target="_blank" rel="noreferrer">Open-source</a>
       {" "} by Patrycja Szumska.
